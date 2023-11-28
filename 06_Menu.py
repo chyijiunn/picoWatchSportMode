0be0abac8ac17134e49fc6b1e544b63b2d1d1331
chyijiunn <chyijiunn@gmail.com>
2023-11-28 12:22:55 +0800

    ud

---
+++ 06_Menu.py
@@ -0,0 +1,20 @@
+from machine import Pin, SPI
+from fonts.romfonts import vga2_16x32 as font
+import gc9a01py as gc9a01
+import random ,utime ,RP
+
+spi = SPI(1, baudrate=62500000, sck=Pin(10), mosi=Pin(11))
+LCD = gc9a01.GC9A01(spi,dc=Pin(8, Pin.OUT),cs=Pin(9, Pin.OUT),reset=Pin(12, Pin.OUT),backlight=Pin(25, Pin.OUT),rotation=0)
+color = gc9a01.color565
+
+LCD.fill(color(0,0,0))
+ori_x , ori_y = 120 ,120 
+while True:
+    xyz = RP.QMI8658().Read_XYZ()
+    LCD.pixel(ori_x+int(50*xyz[1]),ori_y-int(50*xyz[0]),color(255,255,255))
+    ori_x = ori_x+int(xyz[1])
+    ori_y = ori_y - int(xyz[0])
+    
+    if ori_x > 200:
+        from stoptime import *
+        stoptime_main()
